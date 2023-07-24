9acbafe1b3e4caa5d3bfb8967cdcd79385e8485e
ifanuAntoni <ifanoea@gmail.com>
2023-07-25 02:30:31 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 // import Image from "next/image";
 
 export default function Home() {
-  return <div className="max-w-5xl bg-slate-200 mx-auto text-black"> Hai Innu </div>;
+  return <div className="max-w-6xl bg-slate-200 mx-auto text-black"> Hai Innu </div>;
 }
